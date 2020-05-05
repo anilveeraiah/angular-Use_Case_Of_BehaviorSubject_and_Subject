f6e9cda73a8340067d441f07f868800632258eda
anilveeraiah <anilveeraiah@gmail.com>
2020-05-05 15:58:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kkzwyx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kkzwyx)
---
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from "../services/cart.service";
+
+@Component({
+  selector: 'app-shopping-cart',
+  templateUrl: './shopping-cart.component.html',
+  styleUrls: ['./shopping-cart.component.css']
+})
+export class ShoppingCartComponent implements OnInit {
+ items : Array<Object> = [];
+  constructor(private cartService: CartService) {
+    this.cartService.getCartProducts().subscribe(p => {
+      this.items = p;
+    })
+   }
+
+  ngOnInit() {
+  }
+
+  removeFromCart(item) {
+    this.cartService.removeFromCart(item);
+  }
+
+}
